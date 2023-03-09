8b613f01e966bb1e2f86a7e1523f121796ea5f27
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2023-03-08 23:29:41 -0300

    navbar component created

---
+++ src/app/component/layout/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  user = {
+    img:'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    name: 'Matilda'
+  }
+  active = 2;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
