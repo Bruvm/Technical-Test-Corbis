83a55dd2a770c3c436cea9feb637cc728a51bbcc
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2023-03-08 23:33:42 -0300

    added 'bootstrap-icons' packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LayoutComponent } from './component/layout/layout.component';
+import { NavbarComponent } from './component/layout/navbar/navbar.component';
+import { SidebarComponent } from './component/layout/sidebar/sidebar.component';
+import { AsideComponent } from './component/layout/aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavbarComponent,
+    SidebarComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
